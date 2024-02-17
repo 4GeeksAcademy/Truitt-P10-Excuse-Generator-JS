c67533573de6aafb74ca5d7d6cb665172a52b373
Truitt Janney <153782672+truittjanney@users.noreply.github.com>
2024-02-17 01:36:53 +0000

    progress1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let generateExcuse = () => {
+
+  let who = ["the dog" , "My grandma" , "My turtle" , "My bird"];
+
+  let action = ["ate" , "peed" , "crushed" , "broke"];
+
+  let what = ["my homework" , "the keys" , "my car"];
+
+  let when = ["before the class" , "right on time" , "when i finished" , "during my lunch" , "while I was praying"];
+
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let subjIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possessionIndex = Math.floor(Math.random() * possession.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
   console.log("Hello Rigo from the console!");
 };
